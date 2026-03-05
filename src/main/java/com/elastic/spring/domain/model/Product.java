66634bb26db8a3e7f7515bee21a0667249f4a14f
luger-mano <lucas.germanysantos@gmail.com>
2026-03-05 15:39:31 -0300

    [feat] : Entity Product criada com os dados necessários para aparecer no elastic

--- src/main/java/com/elastic/spring/domain/model/Product.java
+++ src/main/java/com/elastic/spring/domain/model/Product.java
@@ -1,4 +1,31 @@
 package com.elastic.spring.domain.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.util.UUID;
+
+@Document(indexName = "product")
+@Getter
+@Setter
 public class Product {
+
+    @Id
+    private UUID productId;
+    private String name;
+    private Double value;
+    private Integer amount;
+
+    public Product(UUID productId, String name, Double value, Integer amount) {
+        this.productId = productId;
+        this.name = name;
+        this.value = value;
+        this.amount = amount;
+    }
+
+    public Product() {
+    }
 }
