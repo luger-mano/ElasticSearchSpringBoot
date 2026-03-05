61e810dc85928f0bede382b8ef1b4e456d8ddbf6
luger-mano <lucas.germanysantos@gmail.com>
2026-03-05 15:40:07 -0300

    [feat] : Controller com rota GET adicionado (ainda vou adicionar a lógica)

--- src/main/java/com/elastic/spring/adapter/controller/ProductController.java
+++ src/main/java/com/elastic/spring/adapter/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.elastic.spring.adapter.controller;
 
+import com.elastic.spring.adapter.dto.ProductResponseDto;
+import com.elastic.spring.domain.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductResponseDto>> products(){
+        return ResponseEntity.ok().build();
+    }
 }
