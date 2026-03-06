e6419269608cf23490def54a75cc6f5d5d06aa87
luger-mano <lucas.germanysantos@gmail.com>
2026-03-06 18:09:48 -0300

    [feat] : Habilitei ElasticsearchRepositories para acessar dados, e configurei ambiente para acesso sem ssl

--- src/main/java/com/elastic/spring/infra/configuration/CustomElasticsearchConfig.java
+++ src/main/java/com/elastic/spring/infra/configuration/CustomElasticsearchConfig.java
@@ -3,15 +3,17 @@ package com.elastic.spring.infra.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories
 public class CustomElasticsearchConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo("localhost:9200")
-                .withBasicAuth("elastic", "steve3571")
+                .usingSsl(false)
                 .build();
     }
 }
