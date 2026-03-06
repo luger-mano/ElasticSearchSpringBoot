5e6528573ba086d06ba8bc4a7e94f64c2855e330
luger-mano <lucas.germanysantos@gmail.com>
2026-03-06 18:15:53 -0300

    [feat] : Dtos response e request

--- src/main/java/com/elastic/spring/adapter/dto/ProductRequestDto.java
+++ src/main/java/com/elastic/spring/adapter/dto/ProductRequestDto.java
@@ -1,4 +1,32 @@
 package com.elastic.spring.adapter.dto;
 
+import com.elastic.spring.domain.model.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductRequestDto {
+    private String name;
+    private Double value;
+    private Integer amount;
+
+    public ProductResponseDto toResponseDto(){
+        ProductResponseDto responseDto = new ProductResponseDto();
+
+        responseDto.setName(this.name);
+        responseDto.setValue(this.value);
+        responseDto.setAmount(this.amount);
+
+        return responseDto;
+    }
+    public Product toEntity(){
+        Product product = new Product();
+
+        product.setName(this.name);
+        product.setValue(this.value);
+        product.setAmount(this.amount);
+
+        return product;
+    }
 }
--- src/main/java/com/elastic/spring/adapter/dto/ProductResponseDto.java
+++ src/main/java/com/elastic/spring/adapter/dto/ProductResponseDto.java
@@ -1,4 +1,23 @@
 package com.elastic.spring.adapter.dto;
 
+import com.elastic.spring.domain.model.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductResponseDto {
+    private String name;
+    private Double value;
+    private Integer amount;
+
+    public Product toEntity(){
+        Product product = new Product();
+
+        product.setName(this.name);
+        product.setValue(this.value);
+        product.setAmount(this.amount);
+
+        return product;
+    }
 }
