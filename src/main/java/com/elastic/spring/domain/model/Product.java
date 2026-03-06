354b3e043c36d961a06a9ad95f818a38f3cf204f
luger-mano <lucas.germanysantos@gmail.com>
2026-03-06 18:14:01 -0300

    [refact] : Adicionei assinatura dos campos para definir a tipagem

--- src/main/java/com/elastic/spring/domain/model/Product.java
+++ src/main/java/com/elastic/spring/domain/model/Product.java
@@ -5,18 +5,25 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.UUID;
 
-@Document(indexName = "product")
+
+
 @Getter
 @Setter
+@Document(indexName = "product")
 public class Product {
 
     @Id
     private UUID productId;
+    @Field(type = FieldType.Text)
     private String name;
+    @Field(type = FieldType.Double)
     private Double value;
+    @Field(type = FieldType.Integer)
     private Integer amount;
 
     public Product(UUID productId, String name, Double value, Integer amount) {
